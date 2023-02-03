134406b0d47efeeac604e7ec67b5e950228446e6
Rashi porwal <89764162+rashi24-p@users.noreply.github.com>
2023-02-03 08:33:52 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const colorBtn = document.querySelector('colorBtn');
+const bodyBcg = document.querySelector('body');
+
+const colors = ["yellow","green","Blue"];
+
+colorBtn.addEventListener('click',changeColor);
+
+function changeColor (){
+  let random = Math.floor
+}
